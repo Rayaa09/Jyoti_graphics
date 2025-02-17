7311f4daa3a6a99eab6f197898dd3e9ac7d1aa23
sanyukta001 <sanyuktamishra911@gmail.com>
2025-02-17 17:07:15 +0530

    changes with css

--- src/App.js
+++ src/App.js
@@ -10,99 +10,107 @@ function App() {
     const navigate = useNavigate();
   return (
     <div className="App">
-      {/* <Welcome /> */}
-    <header>
-      <div className="logo">
-        <img src="/logo.png" alt="" style={{ 
-          height: '60px',
-          marginLeft: '-1000px',
-          marginBottom: '-100px',
-          verticalAlign: 'middle'
-        }} />
-      </div>
-        <h1>Jyoti Graphics</h1>
-                      <p>Your Trusted Printing Partner</p>
-                      <div className="social-icons">
-                      <a href="https://www.instagram.com/jyoti_print" target="_blank" rel="noreferrer">
-                        <FaInstagram size={24} />
-                      </a>
-                      <a href="https://twitter.com" target="_blank" rel="noreferrer">
-                        <FaTwitter size={24} />
-                      </a>
-                      <a href="https://facebook.com" target="_blank" rel="noreferrer">
-                        <FaFacebook size={24} />
-                      </a>
-                      </div>
-    </header>
+        <header>
+          {/* <div className="logo">
+          <img src="/logo.png" alt="" style={{ 
+            height: '60px',
+            marginLeft: '-1000px',
+            marginBottom: '-100px',
+            verticalAlign: 'middle'
+          }} />
+          </div> */}
+          <div className="header-image">
+            <img src="/jyoti-graphics.png" alt="" style={{
+            height: '150px',
+            margin: '20px auto',
+            display: 'block'
+            }} />
+          </div>
+          <p>Your Trusted Printing Partner</p>
+          <div className="social-icons">
+            <a href="https://www.instagram.com/jyoti_print" target="_blank" rel="noreferrer">
+            <FaInstagram size={24} />
+            </a>
+            <a href="https://twitter.com" target="_blank" rel="noreferrer">
+            <FaTwitter size={24} />
+            </a>
+            <a href="https://facebook.com" target="_blank" rel="noreferrer">
+            <FaFacebook size={24} />
+            </a>
+          </div>
+        </header>
 
-    <div class="container">
-        <div class="company">
+        <div class="container">
+          <div class="company">
             <div class="flip-box-front">
-            <h2>Jyoti Graphics - Main Office</h2>
+            <h2>JYOTI GRAPHICS</h2>
             </div>
             <div class="flip-box-back" onClick={() => navigate("/branches")}>
             <p>Click to Open the branches option</p>
             </div>
-    
-        </div>
+        
+          </div>
 
-        <div class="company">
+          <div class="company">
             <div class="flip-box-front">
-            <h2>Jyoti Graphics - Branch 1</h2>
-        </div>
-        <div class="flip-box-back">
+            <h2>JYOTI ENTERPRISE</h2>
+          </div>
+          <div class="flip-box-back">
             <p>
-                <a href="https://maps.app.goo.gl/gB2a4khpiiDSCdsE6" target="_blank" rel="noreferrer">
-                    30A Canal East Road, Kolkata - 700 011
-                </a>
+              <a href="https://maps.app.goo.gl/gB2a4khpiiDSCdsE6" target="_blank" rel="noreferrer">
+                30A Canal East Road, Kolkata - 700 011
+              </a>
             </p>
-        </div>
-        </div>
+          </div>
+          </div>
+          <div class="company">
+                <div class="flip-box-front">
+                <h2>SIGN INK</h2>
+              </div>
+              <div class="flip-box-back">
+                <p>
+                  <a href="https://maps.app.goo.gl/ZpNVH2muyBSiHUEbA" target="_blank" rel="noreferrer">
+                  30A CANAL EAST ROAD, KOLKATA- 700 011
+                  </a>
+                </p>
+              </div>
+            </div>
 
-        <div class="company">
+          <div class="company">
             <div class="flip-box-front">
-            <h2>Jyoti Graphics - Branch 2</h2>
-        </div>
-        <div class="flip-box-back">
+            <h2>AMAN PRINTERS</h2>
+          </div>
+          <div class="flip-box-back">
             <p>
-                <a href="https://maps.app.goo.gl/NqA35seJL1R231Qw6" target="_blank" rel="noreferrer">
-                    35A Surya Sen Street Kolkata -700009
-                </a>
+              <a href="https://maps.app.goo.gl/NqA35seJL1R231Qw6" target="_blank" rel="noreferrer">
+                35A Surya Sen Street Kolkata -700009
+              </a>
             </p>
-        </div>
-        </div>
+          </div>
+          </div>
 
-        <div class="company">
+          <div class="company">
             <div class="flip-box-front">
-            <h2>Jyoti Graphics - Branch 3</h2>
-        </div>
-        <div class="flip-box-back">
+            <h2>JYOTI LASER POINT</h2>
+          </div>
+          <div class="flip-box-back">
             <p>
-                <a href="https://maps.app.goo.gl/yXMrLj4yKeDPpZLn8" target="_blank" rel="noreferrer">
-                    109/1 Beleghata main road, Kolkata -700 010
-                </a>
+              <a href="https://maps.app.goo.gl/yXMrLj4yKeDPpZLn8" target="_blank" rel="noreferrer">
+                109/1 Beleghata main road, Kolkata -700 010
+              </a>
             </p>
+          </div>
         </div>
-    </div>
-    </div>
-    
-    <footer className="bg-gray-800 text-white text-center p-4">
-      <div className="flex justify-center space-x-4 mb-2">
-        {/* <a href="https://www.instagram.com/jyoti_print" target="_blank" rel="noreferrer">
-                        <FaInstagram size={24} />
-                      </a>
-                      <a href="https://twitter.com" target="_blank" rel="noreferrer">
-                        <FaTwitter size={24} />
-                      </a>
-                      <a href="https://facebook.com" target="_blank" rel="noreferrer">
-                        <FaFacebook size={24} />
-                      </a> */}
-                      <div className="foot-social-icons">
-                      <a href="tel:+919123456789">
-                        {/* <i className="fas fa-phone" style={{ fontSize: '24px' }}></i> */}
+        </div>
+        
+        <footer className="bg-gray-800 text-white text-center p-4">
+          <div className="flex justify-center space-x-4 mb-2">
+                  <div className="foot-social-icons">
+                  <a href="tel:+919836267009">
+                  {/* <i className="fas fa-phone" style={{ fontSize: '24px' }}></i> */}
                         <FaPhone size={24} />
                       </a>
-                      <a href="mailto:contact@jyotigraphics.com">
+                      <a href="mailto:jyotigraphics008@gmail.com">
                         {/* <i className="fas fa-envelope" style={{ fontSize: '24px' }}></i> */}
                         <FaMailBulk size={24} />
                       </a>
--- src/Branches.js
+++ src/Branches.js
@@ -91,25 +91,64 @@ function Branches() {
                   </p>
               </div>
           </div>
+          {/* <div class="company">
+                  <div class="flip-box-front">
+                  <h2>JYOTI ENTERPRISE</h2>
+              </div>
+              <div class="flip-box-back">
+                  <p>
+                      <a href="https://maps.app.goo.gl/ZpNVH2muyBSiHUEbA" target="_blank" rel="noreferrer">
+                      30A CANAL EAST ROAD, KOLKATA- 700 011
+                      </a>
+                  </p>
+              </div>
+          </div>
+          <div class="company">
+                  <div class="flip-box-front">
+                  <h2>SIGN INK</h2>
+              </div>
+              <div class="flip-box-back">
+                  <p>
+                      <a href="https://maps.app.goo.gl/ZpNVH2muyBSiHUEbA" target="_blank" rel="noreferrer">
+                      30A CANAL EAST ROAD, KOLKATA- 700 011
+                      </a>
+                  </p>
+              </div>
+          </div>
+          <div class="company">
+                  <div class="flip-box-front">
+                  <h2>AMAN PRINTERS</h2>
+              </div>
+              <div class="flip-box-back">
+                  <p>
+                      <a href="https://maps.app.goo.gl/ZNwejhzHRQEZHgkw6" target="_blank" rel="noreferrer">
+                      109/1 DR. SURESH MUKHERJEE ROAD, KOLKATA-700 010
+                      </a>
+                  </p>
+              </div>
+          </div>
+          <div class="company">
+                  <div class="flip-box-front">
+                  <h2>JYOTI LASER POINT</h2>
+              </div>
+              <div class="flip-box-back">
+                  <p>
+                      <a href="https://maps.app.goo.gl/xjY5rmzyGqqWLM1u8" target="_blank" rel="noreferrer">
+                      63/2D SURYA SEN STREET, KOLKATA- 700 009
+                      </a>
+                  </p>
+              </div>
+          </div> */}
           </div>
           <button onClick={() => navigate('/')} className="back-button">← Back to Home</button>
           <footer className="bg-gray-800 text-white text-center p-4">
             <div className="flex justify-center space-x-4 mb-2">
-              {/* <a href="https://www.instagram.com/jyoti_print" target="_blank" rel="noreferrer">
-                <FaInstagram size={24} />
-              </a>
-              <a href="https://twitter.com" target="_blank" rel="noreferrer">
-                <FaTwitter size={24} />
-              </a>
-              <a href="https://facebook.com" target="_blank" rel="noreferrer">
-                <FaFacebook size={24} />
-              </a> */}
               <div className="foot-social-icons">
-              <a href="tel:+919123456789">
+              <a href="tel:+919836267009">
                 {/* <i className="fas fa-phone" style={{ fontSize: '24px' }}></i> */}
                 <FaPhone size={24} />
               </a>
-              <a href="mailto:contact@jyotigraphics.com">
+              <a href="mailto:jyotigraphics008@gmail.com">
                 {/* <i className="fas fa-envelope" style={{ fontSize: '24px' }}></i> */}
                 <FaMailBulk size={24} />
               </a>
